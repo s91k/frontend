7a610a4f1050f0c731b549371e4fba5e6eb2278c
Kayla Woodbury <59558092+kaylawoodbury@users.noreply.github.com>
2025-04-22 14:34:14 +0200

    add translations and fix clink from overview to learnMore page

--- src/lib/learn-more/articleList.ts
+++ src/lib/learn-more/articleList.ts
@@ -1,7 +1,7 @@
 export type ArticleMeta = {
   id: string;
-  title: string;
-  excerpt: string;
+  titleKey: string;
+  excerptKey: string;
   image?: string;
   link?: string;
   nextArticle?: string[];
@@ -10,16 +10,8 @@ export type ArticleMeta = {
 export const articleMetaData: ArticleMeta[] = [
   {
     id: "reporting",
-    title: "Understanding Climate Reporting",
-    excerpt:
-      "Learn about the importance of climate reporting, its impact on businesses and communities, and how it drives climate action.",
-    image: "/images/blogImages/matthias-heyde-co2-unsplash.jpg",
-  },
-  {
-    id: "why-reporting-matters",
-    title: "Importance of Data Reporting and Transparency",
-    excerpt:
-      "Understand the impact of data collection and transparency on taking effective climate action.",
-    image: "/images/blogImages/matthias-heyde-co2-unsplash.jpg",
+    titleKey: "learnMoreOverview.reporting.title",
+    excerptKey: "learnMoreOverview.reporting.excerpt",
+    image: "/images/learnMoreImages/reporting.jpg",
   },
 ];
--- src/pages/LearnMoreArticle.tsx
+++ src/pages/LearnMoreArticle.tsx
@@ -5,18 +5,14 @@ import { LearnMoreReporting } from "./LearnMoreReporting";
 export function LearnMoreArticle() {
   const { id } = useParams();
 
-  // Find the current article based on the ID
   const currentArticle = articleMetaData.find((article) => article.id === id);
 
-  // If no article is found, return null (or a 404 component)
   if (!currentArticle) {
     return null;
   }
-
-  // Route to the appropriate component based on the article ID
   switch (id) {
     case "reporting":
-      return <LearnMoreReporting article={currentArticle} />;
+      return <LearnMoreReporting />;
     // Add more cases here as you create new article components
     default:
       return null;
