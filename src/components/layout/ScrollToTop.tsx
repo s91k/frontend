903ef36e449259aa589849c76fb968cf5f151b83
Mikael Hallendal <hallski@hallski.org>
2025-05-15 09:54:12 +0200

    Ensure scroll to top is above data guide
    When page is scrolled and data guide is visable, put scroll to top
    button on top.

--- src/components/layout/ScrollToTop.tsx
+++ src/components/layout/ScrollToTop.tsx
@@ -29,7 +29,7 @@ export function ScrollToTop() {
       {isVisible && (
         <button
           onClick={scrollToTop}
-          className="fixed bottom-6 right-6 p-3 bg-black-1 hover:bg-black-2 text-white rounded-full shadow-lg transition-colors duration-200"
+          className="fixed bottom-6 right-6 p-3 bg-black-1 hover:bg-black-2 text-white rounded-full shadow-lg transition-colors duration-200 z-[35]"
           aria-label="Scroll to top"
         >
           <ChevronUp size={24} />
