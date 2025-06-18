17a58237c19bafb3c91dea35cd84fcf8154a72b7
Mikael Hallendal <hallski@hallski.org>
2025-06-18 14:57:29 +0200

    Make the search button look less disabled
    Feedback from UX to make the search icon white to make it look more
    enabled.

--- src/components/search/HeaderSearchButton.tsx
+++ src/components/search/HeaderSearchButton.tsx
@@ -52,13 +52,13 @@ export const HeaderSearchButton = ({
         className={cn(
           "px-2 py-1 bg-black-1 h-8",
           "flex items-center gap-2",
-          "text-white/30 hover:text-white/60 hover:bg-white/20",
+          "text-white hover:text-white/60 hover:bg-white/20",
           "rounded-full border border-grey/20 hover:border-gray/40 transition-colors ",
           className,
         )}
       >
         <SearchIcon className="h-4 w-4 mx-1" />
-        <span className="mr-2 text-sm">
+        <span className="mr-2 text-sm text-grey">
           {t("globalSearch.headerButtonTitle", "Search")}
         </span>
       </button>
