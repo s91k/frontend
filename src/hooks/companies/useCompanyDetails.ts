ce92e2e8c321cf41d84bf233e25d4ba65bbc0a73
Mikael Hallendal <hallski@hallski.org>
2025-06-19 10:41:32 +0200

    Remove unused type definition in useCompanyDetails

--- src/hooks/companies/useCompanyDetails.ts
+++ src/hooks/companies/useCompanyDetails.ts
@@ -1,11 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { getCompanyDetails } from "@/lib/api";
-import type { paths } from "@/lib/api-types";
 import { cleanEmissions } from "@/utils/cleanEmissions";
-
-type CompanyDetails = NonNullable<
-  paths["/companies/{wikidataId}"]["get"]["responses"][200]["content"]["application/json"]
->;
 
 export function useCompanyDetails(id: string) {
   const {
