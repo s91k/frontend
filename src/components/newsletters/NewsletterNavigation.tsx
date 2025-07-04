e70336575d2710fc193201386ab822163db5788c
Carl <carlwiren90@gmail.com>
2025-07-04 15:17:38 +0200

    Added first pagination iteration

--- src/components/newsletters/NewsletterNavigation.tsx
+++ src/components/newsletters/NewsletterNavigation.tsx
@@ -3,6 +3,11 @@ import { useState, useEffect, useRef } from "react";
 import { useScreenSize } from "@/hooks/useScreenSize";
 import { NewsletterType } from "@/lib/newsletterArchive/newsletterData";
 import { useNavigate } from "react-router-dom";
+import {
+  Pagination,
+  PaginationNext,
+  PaginationPrevious,
+} from "../ui/pagination";
 
 interface NewsletterNavigationProps {
   newsletterList: Array<T>;
@@ -18,38 +23,52 @@ export function NewsletterNavigation({
   const { t } = useTranslation();
   const { isMobile } = useScreenSize();
   const navigate = useNavigate();
-  console.log(newsletterList);
+  const [displayedNavLinks, setDisplayedNavLinks] = useState();
+
+  useEffect(() => {
+    if (!newsletterList) {
+      return;
+    } else {
+      setDisplayedNavLinks(newsletterList.slice(0, 5));
+    }
+  }, [newsletterList]);
 
   return (
     <nav
       aria-label="Methodology Navigation"
-      className={`${isMobile ? "max-h-80" : "max-h-[570px]"} bg-black-2 rounded-md p-2 lg:max-w-[280px] overflow-y-scroll`}
+      className={`${isMobile ? "max-h-80" : "max-h-[570px]"} flex flex-col bg-black-2 rounded-md p-2 lg:max-w-[280px] justify-between`}
     >
-      <h2 className="sr-only">{t("methodsPage.dataSelector.label")}</h2>
+      <div>
+        <h2 className="sr-only">{t("methodsPage.dataSelector.label")}</h2>
 
-      <ul className="divide-y divide-black-1">
-        {newsletterList?.map((newsletter, index) => {
-          return (
-            <li key={index}>
-              <button
-                onClick={() => {
-                  setDisplayedNewsletter?.(newsletter.long_archive_url);
-                  navigate(`?view=${newsletter.id}`);
-                }}
-                className="flex flex-col gap-[5px] justify-between items-left w-full p-3 my-1 text-left text-sm font-medium text-grey hover:bg-black-1 transition-colors duration-200 rounded-lg"
-              >
-                <span className="text-sm font-medium text-white">
-                  {newsletter.send_time.slice(
-                    0,
-                    newsletter.send_time.indexOf("T"),
-                  )}
-                </span>
-                <span>{newsletter.settings.subject_line}</span>
-              </button>
-            </li>
-          );
-        })}
-      </ul>
+        <ul className="divide-y divide-black-1">
+          {displayedNavLinks?.map((newsletter, index) => {
+            return (
+              <li key={index}>
+                <button
+                  onClick={() => {
+                    setDisplayedNewsletter?.(newsletter.long_archive_url);
+                    navigate(`?view=${newsletter.id}`);
+                  }}
+                  className="flex flex-col gap-[5px] justify-between items-left w-full p-3 my-1 text-left text-sm font-medium text-grey hover:bg-black-1 transition-colors duration-200 rounded-lg"
+                >
+                  <span className="text-sm font-medium text-white">
+                    {newsletter.send_time.slice(
+                      0,
+                      newsletter.send_time.indexOf("T"),
+                    )}
+                  </span>
+                  <span>{newsletter.settings.subject_line}</span>
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <Pagination>
+        <PaginationPrevious></PaginationPrevious>
+        <PaginationNext></PaginationNext>
+      </Pagination>
     </nav>
   );
 }
