c1e1931ecbba3b8e1da285bf261dea350e237d18
Kayla Woodbury <59558092+kaylawoodbury@users.noreply.github.com>
2025-08-13 07:50:49 -0700

    Revert "Revert "[652] Added Newsletter Archive page"" (#925)

---
+++ src/components/newsletters/NewsletterNavigation.tsx
@@ -0,0 +1,95 @@
+import { useState, useEffect } from "react";
+import { useScreenSize } from "@/hooks/useScreenSize";
+import { NewsletterType } from "@/lib/newsletterArchive/newsletterData";
+import { useNavigate } from "react-router-dom";
+import itemPagination from "@/utils/itemPagination";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "../ui/pagination";
+import { Page } from "@/utils/itemPagination";
+
+interface NewsletterNavigationProps {
+  newsletterList: Array<NewsletterType>;
+  setDisplayedNewsletter: (newsletter: NewsletterType) => void;
+  displayedNewsLetter: NewsletterType | null;
+}
+
+export function NewsletterNavigation({
+  newsletterList,
+  displayedNewsLetter,
+  setDisplayedNewsletter,
+}: NewsletterNavigationProps) {
+  const { isMobile } = useScreenSize();
+  const navigate = useNavigate();
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [paginatedContent, setPaginatedContent] = useState<Record<
+    string,
+    Page<NewsletterType>
+  > | null>(null);
+
+  useEffect(() => {
+    if (newsletterList) {
+      const paginatedItems = itemPagination({
+        content: newsletterList,
+      });
+      if (paginatedItems) {
+        setPaginatedContent(paginatedItems);
+      }
+    }
+  }, [newsletterList]);
+
+  return (
+    <>
+      {paginatedContent && (
+        <Pagination
+          className={`${isMobile ? "h-[575px] p-2" : "h-[585px] p-2"} flex flex-col bg-black-2 rounded-md lg:max-w-[280px] justify-between`}
+        >
+          <PaginationContent
+            className={`${isMobile ? "min-h-[300px]" : "h-3/4"} flex flex-col divide-y divide-black-1 items-baseline text-left`}
+          >
+            {paginatedContent &&
+              paginatedContent["page" + currentPage].items.map(
+                (item, index) => {
+                  return (
+                    <PaginationLink
+                      key={index}
+                      className={`
+                      ${displayedNewsLetter?.long_archive_url === item.long_archive_url ? "bg-black-1" : null}
+                      ${isMobile ? "max-h-[100px]" : "max-h-[125px]"}
+                       flex flex-col h-full gap-[5px] w-full p-3 my-1 text-left items-start text-sm font-medium text-grey hover:bg-black-1 transition-colors cursor-pointer duration-200 rounded-lg`}
+                      onClick={() => {
+                        setDisplayedNewsletter?.(item);
+                        navigate(`?view=${item.id}`);
+                      }}
+                    >
+                      <span
+                        className="flex text-sm font-medium text-white
+"
+                      >
+                        {item.send_time.slice(0, item.send_time.indexOf("T"))}
+                      </span>
+                      {item.settings.subject_line}
+                    </PaginationLink>
+                  );
+                },
+              )}
+          </PaginationContent>
+          <div className="flex justify-center mt-4">
+            {paginatedContent["page" + currentPage].hasPreviousPage && (
+              <PaginationPrevious
+                onClick={() => setCurrentPage(currentPage - 1)}
+              />
+            )}
+            {paginatedContent["page" + currentPage].hasNextPage && (
+              <PaginationNext onClick={() => setCurrentPage(currentPage + 1)} />
+            )}
+          </div>
+        </Pagination>
+      )}
+    </>
+  );
+}
---
+++ src/hooks/useNewsletters.ts
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchNewsletters } from "@/lib/api";
+import { NewsletterType } from "@/lib/newsletterArchive/newsletterData";
+
+export const useNewsletters = () => {
+  const { data, isLoading, isError } = useQuery({
+    queryFn: fetchNewsletters,
+    queryKey: ["newsletters"],
+    select: (data) =>
+      data.campaigns
+        .filter((campaign: NewsletterType) => campaign.send_time?.trim() !== "")
+        .reverse(),
+  });
+
+  return {
+    data: data || [],
+    isLoading,
+    isError,
+  };
+};
