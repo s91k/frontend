91685b1a518b633bb10c7edf0d89ed63654e896e
Carl <carlwiren90@gmail.com>
2025-08-20 07:44:40 +0200

    Changed sticky title to be shown after company/municipality is out of sight

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -38,7 +38,7 @@ export function Header() {
 
   useEffect(() => {
     const onScroll = () => {
-      if (window.scrollY >= 200) {
+      if (window.scrollY >= 125) {
         setShowTitle(true);
       } else {
         setShowTitle(false);
@@ -126,7 +126,10 @@ export function Header() {
       sublinks: [
         { label: t("header.reports"), path: `${currentLanguage}/reports` },
         { label: t("header.articles"), path: `${currentLanguage}/articles` },
-        { label: t("header.newsletterArchive"), path: `${currentLanguage}/newsletter-archive`},
+        {
+          label: t("header.newsletterArchive"),
+          path: `${currentLanguage}/newsletter-archive`,
+        },
         { label: t("header.learnMore"), path: `${currentLanguage}/learn-more` },
       ],
     },
